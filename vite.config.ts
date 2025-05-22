4860e3076b2122c9afa3a66cb49eb740c7f0782a
tanasibob71 <tanasibob71@gmail.com>
2025-05-22 12:49:25 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default async () => {
   const plugins = [react(), runtimeErrorOverlay()];
 
-  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
+  // Optionally include cartographer plugin
+  if (
+    process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+  ) {
     const { cartographer } = await import("@replit/vite-plugin-cartographer");
     plugins.push(cartographer());
   }
@@ -15,15 +19,18 @@ export default async () => {
     plugins,
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "client", "src"),
-        "@shared": path.resolve(__dirname, "shared"),
-        "@assets": path.resolve(__dirname, "attached_assets"),
+        "@": path.resolve(import.meta.dirname, "client", "src"),
+        "@shared": path.resolve(import.meta.dirname, "shared"),
+        "@assets": path.resolve(import.meta.dirname, "attached_assets"),
       },
     },
-    root: path.resolve(__dirname, "client"),
+    root: path.resolve(import.meta.dirname, "client"),
     build: {
-      outDir: path.resolve(__dirname, "dist/public"),
+      outDir: path.resolve(import.meta.dirname, "dist/public"),
       emptyOutDir: true,
+    },
+    esbuild: {
+      tsconfigRaw: require("./client/tsconfig.client.json"), // ? ensures JSX is recognized
     },
   });
 };
