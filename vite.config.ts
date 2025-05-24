cf495bc4a546fc8ca64444fe003e2c181a28834c
tanasibob71 <tanasibob71@gmail.com>
2025-05-24 05:44:55 -0400

    next

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client'),
-      '@shared': path.resolve(__dirname, 'shared')
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
